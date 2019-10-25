858b5f1ee14c00c2d36718d2cd718e0749b9c3ad
Oda Kiese <odakiese@Odas-MacBook-Pro.local>
2019-10-25 14:27:09 +0200

    Added some code that attempts to open the .pg file. None of it works.

--- GAN/open_pb.py
+++ GAN/open_pb.py
@@ -3,4 +3,14 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 
-new_model = tf.keras.models.load_model('40000')
+model_name = '40000'
+with tf.compat.v1.Session()(graph=tf.Graph()) as sess:
+    tf.saved_model.loader.load(sess, ["serve"], model_name)
+
+with tf.Graph().as_default():
+    tf.random.set_seed(42)
+
+with tf.compat.v1.Session() as sess:
+    sess.run(tf.contrib.tpu.initialize_system())
+
+new_model = tf.keras.models.load_model(filepath=model_name)
