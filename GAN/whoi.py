d420b10f6b77fc20a79f85be0e2ad32470889235
Oda Kiese <odakiese@dhcp-10-22-67-69.wlan.ntnu.no>
2019-12-02 18:16:40 +0100

    Added whoi.py script which is added to the tensorflow_dataset dir.

--- GAN/whoi.py
+++ GAN/whoi.py
@@ -1,61 +1,101 @@
-import os
+"""WHOI dataset."""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import cv2
+
+import numpy as np
+import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
 
-class MyDataset(tfds.core.GeneratorBasedBuilder):
-  """Short description of my dataset."""
+_CITATION = """\
+@article{orenstein2015whoi,
+  title={Whoi-plankton-a large scale fine grained visual recognition benchmark dataset for plankton classification},
+  author={Orenstein, Eric C and Beijbom, Oscar and Peacock, Emily E and Sosik, Heidi M},
+  journal={arXiv preprint arXiv:1510.00745},
+  year={2015},
+}"""
+
+_DESCRIPTION = """WHOI dataset for Specialization Project of Oda Kiese, NTNU"""
+
+
+class Whoi(tfds.core.GeneratorBasedBuilder):
+  """WHOI dataset."""
 
   VERSION = tfds.core.Version('0.1.0')
 
   def _info(self):
-    # Specifies the tfds.core.DatasetInfo object
     return tfds.core.DatasetInfo(
         builder=self,
-        # This is the description that will appear on the datasets page.
-        description=("This is the dataset for training a GAN on plankton. It contains images of plankton. The "
-                     "images are kept at their original dimensions."),
-        # tfds.features.FeatureConnectors
+        description=_DESCRIPTION,
         features=tfds.features.FeaturesDict({
-            "image_description": tfds.features.Text(),
-            "image": tfds.features.Image(),
-            # Here, labels can be of 5 distinct values.
-            "label": tfds.features.ClassLabel(num_classes=103),
+            "image": tfds.features.Image(shape=(128, 128, 3)),
+            "label": tfds.features.ClassLabel(num_classes=5),
         }),
-        # If there's a common (input, target) tuple from the features,
-        # specify them here. They'll be used if as_supervised=True in
-        # builder.as_dataset.
         supervised_keys=("image", "label"),
-        # Homepage of the dataset for documentation
-        homepage="https://hdl.handle.net/1912/7350",
-        # Bibtex citation for the dataset
-        #TODO: fix this citation
-        citation=r"""@article{DatasetImage: Sosik, Heidi M., Peacock, Emily E., Brownlee, Emily F.,
-                            "2014 labeled IFCB images", 2014, https://hdl.handle.net/1912/7350"}""",
+        urls=['https://hdl.handle.net/10.1575/1912/7341'],
+        citation=_CITATION,
     )
 
+
   def _split_generators(self, dl_manager):
-    # Downloads the data and defines the splits
-    # dl_manager is a tfds.download.DownloadManager that can be used to
-    # download and extract URLs
-    # Specify the splits
+    """Returns SplitGenerators."""
+    label_file = "batches.meta.txt"
+    whoi_path = "/Users/odakiese/tensorflow_datasets/whoi_org"
+    
+    # Load the label names
+    labels_path = "{}/{}".format(whoi_path, label_file)
+    with tf.io.gfile.GFile(labels_path) as label_f:
+        label_names = [name for name in label_f.read().split("\n") if name]
+    self.info.features["label"].names = label_names
+    
+    # Define the splits
+    def gen_filenames(splitdir):
+        for label in os.listdir("{}/{}".format(whoi_path, splitdir)):
+            if label != ".DS_Store":
+                yield os.path.join(whoi_path, splitdir, label)
+    
     return [
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
-            gen_kwargs={
-                "images_dir_path": os.path.join(extracted_path, "train"),
-                "labels": os.path.join(extracted_path, "train_labels.csv"),
-            },
-        ),
+            num_shards=9,
+            gen_kwargs={"filepaths": gen_filenames("train")}),
         tfds.core.SplitGenerator(
             name=tfds.Split.TEST,
-            gen_kwargs={
-                "images_dir_path": os.path.join(extracted_path, "test"),
-                "labels": os.path.join(extracted_path, "test_labels.csv"),
-            },
-        ),
+            num_shards=1,
+            gen_kwargs={"filepaths": gen_filenames("val")})
     ]
 
-  def _generate_examples(self):
-    # Yields examples from the dataset
-    yield 'key', {}
+  def _generate_examples(self, filepaths):
+    index = 0
+    label_dict = dict(zip(self.info.features["label"].names, [i for i in range(len(self.info.features["label"].names))]))
+    for path in filepaths:
+        label_name = path.split("/")[-1]
+        for img in os.listdir(path):
+            img_path = "{}/{}".format(path, img)
+            for labels, np_image in _load_data(label_name, label_dict, img_path):
+                record = dict(zip(["label"], labels))
+                record["image"] = np_image
+                yield index, record
+                index += 1
 
+def _load_data(label_name, label_dict, path):
+    """Yields (labels, np_image) tuples."""
+    img = np.array(cv2.imread(path)
+            .reshape((3, 128, 128))
+            .transpose(1, 2, 0))
+    label = np.array([label_dict[label_name]])
+    yield label, img
+    
+    """
+    with tf.io.gfile.GFile(path, "rb") as f:
+        data = f.read()
+        img = (np.frombuffer(data, dtype=np.uint8)
+               .reshape((3, 128, 128))
+               .transpose((1, 2, 0)))
+        label = np.array(label_dict[label_name])
+        yield labels, img
+    """
